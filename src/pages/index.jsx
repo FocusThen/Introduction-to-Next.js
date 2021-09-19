deb115b051c87fb10deffa48d86c6b644194d1b2
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2021-09-20 00:16:50 +0300

    feat: next.js Link navigation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 function HomePage() {
-  return <div>Home page</div>
+  return (
+    <div>
+      Home page
+      <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+    </div>
+  )
 }
 
 export default HomePage
--- src/pages/notes/index.jsx
+++ src/pages/notes/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Notes() {
-  return <div>Notes home</div>
+  return (
+    <div>
+      Notes home
+      <Link href="/notes/[id]" as={'/notes/1'}>
+        <a>Note 1</a>
+      </Link>
+    </div>
+  )
 }
 
 export default Notes
