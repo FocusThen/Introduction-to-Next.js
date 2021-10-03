7f64770d729c4716c8c1f6ae44266fb083ff3397
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2021-10-04 01:26:48 +0300

    feat: getServerSideProps example.

--- src/pages/notes/index.jsx
+++ src/pages/notes/index.jsx
@@ -3,11 +3,7 @@
 import { jsx } from 'theme-ui'
 import Link from 'next/link'
 
-const Notes = () => {
-  const notes = new Array(15)
-    .fill(1)
-    .map((e, i) => ({ id: i, title: `This is my note ${i}` }))
-
+const Notes = ({ notes }) => {
   return (
     <div sx={{ variant: 'containers.page' }}>
       <h1>My Notes</h1>
@@ -36,3 +32,12 @@ const Notes = () => {
   )
 }
 export default Notes
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/notes`)
+  const { data } = await res.json()
+
+  return {
+    props: { notes: data },
+  }
+}
